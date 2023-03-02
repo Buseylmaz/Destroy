63069b73545335f928a325e43169e112ae7f8c08
Buse Yılmaz <65345966+Buseylmaz@users.noreply.github.com>
2023-03-02 21:09:31 +0300

    Add files via upload

---
+++ SphereManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereManager : MonoBehaviour
+{
+    public int hasarDurum;
+    void Hasar(int hasarMiktar)
+    {
+        hasarDurum -= hasarMiktar;
+
+        if (hasarDurum < 1)
+        {
+            hasarDurum = 0;
+            Destroy(this.gameObject);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Hasar(5);
+        }
+    }
+}
